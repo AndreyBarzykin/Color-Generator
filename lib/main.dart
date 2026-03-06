168a00ad4433c2e5a914114b42c82ac150f64aea
AndreyBarzykin <andreybarzykin.work@gmail.com>
2026-03-06 12:32:02 +0200

    fix: linter warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_match_file_name
 import 'package:color_generator/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,14 +6,16 @@ void main() {
   runApp(const MyApp());
 }
 
+/// The main entry point for the Color Generator application.
 class MyApp extends StatelessWidget {
+  /// Creates a [MyApp] widget.
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Color Generator',
       theme: ThemeData(),
       home: const HomePage(),
     );
